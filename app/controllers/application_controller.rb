8b8fa8dcd9569f39f3dc1f7e971dc1f7decc6f9a
gomayonqui <escribimepues@gmail.com>
2015-09-06 19:13:58 -0500

    Add admin attribute to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  layout :which_layout
+
+  def which_layout
+    if current_user && current_user.admin?
+      'admin'
+    else
+      'application'
+    end
+  end
+
 end
